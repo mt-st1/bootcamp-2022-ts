0ecbce88c347f33abe4e0f6bab7f0c875606e467
石山　琢己 <01046355@CA8923.local>
2022-04-13 14:42:14 +0900

    Apply prettier & fix a bit

--- src/index.ts
+++ src/index.ts
@@ -10,70 +10,70 @@ type Item = {
 
 const items: Item[] = [
   {
-    name: "name",
-    label: "お名前",
-    tagName: "input",
-    type: "text",
-    placeholder: "例）山田　太郎",
+    name: 'name',
+    label: 'お名前',
+    tagName: 'input',
+    type: 'text',
+    placeholder: '例）山田　太郎',
   },
   {
-    name: "email",
-    label: "メールアドレス",
-    tagName: "input",
-    type: "email",
+    name: 'email',
+    label: 'メールアドレス',
+    tagName: 'input',
+    type: 'email',
     placeholder: `例）example@gmail.com`,
   },
   {
-    name: "tel",
-    label: "電話番号",
-    tagName: "input",
-    type: "tel",
-    placeholder: "例）080-1234-5678",
+    name: 'tel',
+    label: '電話番号',
+    tagName: 'input',
+    type: 'tel',
+    placeholder: '例）080-1234-5678',
   },
   {
-    name: "address",
-    label: "ご住所",
-    tagName: "input",
-    type: "text",
-    placeholder: "例）東京都千代田区丸の内1丁目9-2",
+    name: 'address',
+    label: 'ご住所',
+    tagName: 'input',
+    type: 'text',
+    placeholder: '例）東京都千代田区丸の内1丁目9-2',
   },
   {
-    name: "contact",
-    label: "ご希望の返信方法",
-    tagName: "input",
-    type: "radio",
+    name: 'contact',
+    label: 'ご希望の返信方法',
+    tagName: 'input',
+    type: 'radio',
     values: [
-      { label: "メール", value: 0 },
-      { label: "電話", value: 1 },
-      { label: "どちらでも可", value: 2 },
+      { label: 'メール', value: 0 },
+      { label: '電話', value: 1 },
+      { label: 'どちらでも可', value: 2 },
     ],
   },
   {
-    name: "time",
-    label: "連絡可能な時間帯（電話）",
-    tagName: "input",
-    type: "checkbox",
+    name: 'time',
+    label: '連絡可能な時間帯（電話）',
+    tagName: 'input',
+    type: 'checkbox',
     values: [
-      { label: "09:00〜12:00", value: 0 },
-      { label: "13:00〜16:00", value: 1 },
-      { label: "16:00〜19:00", value: 2 },
+      { label: '09:00〜12:00', value: 0 },
+      { label: '13:00〜16:00', value: 1 },
+      { label: '16:00〜19:00', value: 2 },
     ],
   },
   {
-    name: "inquiry_kind",
-    label: "お問い合せの種類",
-    tagName: "select",
+    name: 'inquiry_kind',
+    label: 'お問い合せの種類',
+    tagName: 'select',
     options: [
-      { text: "返品について", value: 0 },
-      { text: "発送について", value: 1 },
-      { text: "その他", value: 2 },
+      { text: '返品について', value: 0 },
+      { text: '発送について', value: 1 },
+      { text: 'その他', value: 2 },
     ],
   },
   {
-    name: "inquiry_detail",
-    label: "お問い合せ内容",
-    tagName: "textarea",
-    placeholder: "例）お問い合わせ内容詳細をご記入ください",
+    name: 'inquiry_detail',
+    label: 'お問い合せ内容',
+    tagName: 'textarea',
+    placeholder: '例）お問い合わせ内容詳細をご記入ください',
   },
 ];
 
@@ -121,21 +121,23 @@ function createTable() {
   const list = items
     .map((item) => {
       switch (item.tagName) {
-        case "input":
+        case 'input':
           return createInputRow(item);
-        case "select":
+        case 'select':
           return createSelectRow(item);
-        case "textarea":
+        case 'textarea':
           return createTextAreaRow(item);
       }
     })
-    .join("");
+    .join('');
   return `<table>${list}</table>`;
 }
 
 function createFormDom() {
-  const form = document.getElementById("form");
-  form.innerHTML = createTable();
+  const form = document.getElementById('form');
+  if (form !== null) {
+    form.innerHTML = createTable();
+  }
 }
 
 createFormDom();
